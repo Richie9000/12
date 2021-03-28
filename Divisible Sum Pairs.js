505898b536b4cc3990cd5cc42332e3d60e2a893a
gabrielyea <gabriel.gabrielyea@gmail.com>
2021-03-27 21:16:47 -0600

    Update Divisible Sum Pairs.js

--- Divisible Sum Pairs.js
+++ Divisible Sum Pairs.js
@@ -1,27 +1,24 @@
-// Complete the divisibleSumPairs function below.
 function divisibleSumPairs(n, k, ar) {
-let counter = 0;
-for(let i = 0; i < ar.length; i++){
-    for(let j = i + 1; j < ar.length; j++){
-        if(i === j){
+    let counter = 0;
+    for(let i = 0; i < ar.length; i++){
+        for(let j = i + 1; j < ar.length; j++){
+            console.log(i, j);
+            if(isDivisible(ar[i] + ar[j], k)){ //aqui teniamos (isDivisible(i + j, k) !!!
+                                                //estabamos sumando los indices en vez de los valores                                                
+            console.log("sum",ar[i],ar[j])
+            counter++
+            
+            }
             
         }
-        else if(isDivisible(i + j, k)){
-          console.log("sum",ar[i],ar[j])
-          counter++
-        
-        }
-        
     }
-}
-console.log(counter)
-return counter 
+    console.log(counter)
+    return counter 
 }
 
 function isDivisible (num,k){
     
-    if(num % k == 0){
-        console.log(num,k)
+    if(num % k === 0){
         return true
     }
     return false
